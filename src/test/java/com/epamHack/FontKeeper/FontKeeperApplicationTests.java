6939e69cd7297b9ca6176edfa3285879f7a1f2d6
Elena Nyiazova <LihachevaElena.8@mail.ru>
2021-10-08 17:18:56 +0300

    Add files via upload

---
+++ src/test/java/com/epamHack/FontKeeper/FontKeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epamHack.FontKeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FontKeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
