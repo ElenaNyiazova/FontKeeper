98f54018412b13bb87c791b0d270ec8488e233b0
Elena Niiazova <Elena_Niiazova@epam.com>
2021-10-09 08:37:48 +0300

    added Font, Letter, Rule, RuleCategories

--- src/main/java/com/epamHack/FontKeeper/model/db/Letter.java
+++ src/main/java/com/epamHack/FontKeeper/model/db/Letter.java
@@ -1,6 +1,18 @@
 package com.epamHack.FontKeeper.model.db;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
 public class Letter {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String alphabet;
