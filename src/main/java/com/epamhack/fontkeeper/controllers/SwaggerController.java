a246740b5d85e553272d5a0452d144c1742eb6cd
Elena Niiazova <Elena_Niiazova@epam.com>
2021-10-09 18:23:43 +0300

    add some Swagger things again)

--- src/main/java/com/epamhack/fontkeeper/controllers/SwaggerController.java
+++ src/main/java/com/epamhack/fontkeeper/controllers/SwaggerController.java
@@ -1,5 +1,17 @@
 package com.epamhack.fontKeeper.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import static com.epamhack.fontkeeper.constants.EndPoints.SWAGGER_URL;
+import static com.epamhack.fontkeeper.constants.EndPoints.SWAGGER_URL_REDIRECT;
+
+
+@Controller
 public class SwaggerController {
 
+    @RequestMapping(value = SWAGGER_URL_REDIRECT)
+    public String swaggerHtml() {
+        return "redirect:" + SWAGGER_URL;
+    }
 }
